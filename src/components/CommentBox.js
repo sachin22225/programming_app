3b3925ca7929109426038f6412e1b636431a8474
abhishek-shukla-1 <shukla1997abhishek@gmail.com>
2021-02-14 02:36:18 +0530

    Load more btn in commnents + Likes Count Formatter

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -7,6 +7,7 @@ import CommentNew from "./CommentNew"
 export class CommentBox extends Component {
     constructor() {
         super();
+
         //input textBox ref
         this.inputTBCommentRef = React.createRef();
 
@@ -15,7 +16,7 @@ export class CommentBox extends Component {
                 {
                     id: 1,
                     datetime: "Dec 30, 2020",
-                    userName: "Abhishek Shukla",
+                    userName: "Abhishek Shukla10",
                     likes: 5,
                     isLiked: false,
                     commnet: "Hello There !"
@@ -24,8 +25,8 @@ export class CommentBox extends Component {
                 {
                     id: 2,
                     datetime: "Jan 10, 2021",
-                    userName: "Ankit",
-                    likes: 50,
+                    userName: "Ankit9",
+                    likes: 17,
                     isLiked: true,
                     commnet: "This is a comment box prototype"
 
@@ -33,7 +34,7 @@ export class CommentBox extends Component {
                 {
                     id: 3,
                     datetime: "Jan 30, 2021",
-                    userName: "Rahul Pandey",
+                    userName: "Rahul Pandey8",
                     likes: 60,
                     isLiked: false,
                     commnet: "Can't find how to put reply feature !! :-("
@@ -42,15 +43,72 @@ export class CommentBox extends Component {
                 {
                     id: 4,
                     datetime: "Feb 5, 2021",
-                    userName: "Aditya Pratap Singh",
+                    userName: "Aditya Pratap Singh7",
                     likes: 200,
                     isLiked: true,
                     commnet: "Whether you’re a product manager innovating within a larger company, or building a brand new early-stage product at a startup, the JTBD framework works to create better, non-obvious insights about your audience. Ultimately, the core value of this framework is that it provides an approach to gathering an understanding of who your user is, and what their motivations and hopes are. You as the founder or product leader are left to determine how to translate this into what matters most for your product — combining great intuition with great information gathering to make better bets on where to invest resources."
 
+                },
+
+                {
+                    id: 5,
+                    datetime: "Feb 1, 2021",
+                    userName: "Rahul Kr.6",
+                    likes: 1501,
+                    isLiked: false,
+                    commnet: "Hi ! Rahul here."
+
+                },
+                {
+                    id: 6,
+                    datetime: "Feb 1, 2021",
+                    userName: "Priya Singh5",
+                    likes: 0,
+                    isLiked: false,
+                    commnet: "Useful !!"
+
+                },
+                {
+                    id: 7,
+                    datetime: "Feb 10, 2021",
+                    userName: "Rishabh Verma4",
+                    likes: 10000000,
+                    isLiked: true,
+                    commnet: "Uneasy leis the head, that wears the crown !"
+
+                },
+                {
+                    id: 8,
+                    datetime: "Feb 11, 2021",
+                    userName: "MVRSVLN Shastry3",
+                    likes: 257,
+                    isLiked: true,
+                    commnet: "Nice work beta !"
+
+                },
+                {
+                    id: 9,
+                    datetime: "Feb 12, 2021",
+                    userName: "Vivek2",
+                    likes: 123456,
+                    isLiked: false,
+                    commnet: "Amazing Content..."
+
+                },
+                {
+                    id: 10,
+                    datetime: "Feb 12, 2021",
+                    userName: "Abhishek Singh1",
+                    likes: 1000,
+                    isLiked: false,
+                    commnet: "Sunita Mohanty, a Product Lead in Facebook's New Product Experimentation group JTBD is by no means a new way of thinking. But it can be confusing to get started with, since it’s heavy on the corporate strategy jargon and has been reinvented many times over. A quick Google search reveals a bevy of confusing terms, from the debate over jobs-as-progress and jobs-as-activities, or the competing visualizations of maps and hierarchies. I’ve also found that JTBD has a bit of a consulting-esque vibe and seems less vision-driven, which can be off-putting to many product-driven founders. If you’re looking for a deep-dive into the jobs-to-be-done theory and how others apply it, I recommend reading through this primer from The Christensen Institute, this article from Harvard Business Review, Alan Klement’s overview of two different interpretations and Intercom’s guide. But if you’re looking for something a bit more lightweight and accessible for startup product teams, read on for my simplified approach to JTBD."
+
                 }
             ],
 
-            modelShow: false
+            modelShow: false,
+            noOfCommentsDisplayed: 3
+
         }
     }
 
@@ -132,10 +190,19 @@ export class CommentBox extends Component {
 
     }
 
+    loadMoreComments = () => {
+        this.setState({
+            noOfCommentsDisplayed: this.state.noOfCommentsDisplayed + 3
+        })
+    }
 
     printALLComments = () => {
         let temp = [];
-        for (var i = this.state.allComments.length - 1; i >= 0; i--)
+        let count = 0;
+
+        for (let i = this.state.allComments.length - 1; i >= 0; i--, count++) {
+            if (this.state.noOfCommentsDisplayed === count)
+                break;
             temp.push(
                 <CommentNew key={i}
                     index={i}
@@ -147,6 +214,8 @@ export class CommentBox extends Component {
                     updateLikeCount={this.updateLikeCount}
                 />
             );
+        }
+
 
         // temp = this.state.allComments.map((ele, index) => {
         //     return (<GetComment key={index} text={ele} />);
@@ -210,6 +279,19 @@ export class CommentBox extends Component {
                         {this.printALLComments()}
                     </div>
 
+                    <Button
+                        className="loadMoreButton"
+                        variant="primary"
+                        size="lg"
+                        onClick={this.loadMoreComments}
+                        style={{
+                            display:
+                                this.state.noOfCommentsDisplayed >= this.state.allComments.length
+                                    ? "none"
+                                    : "block"
+                        }}
+                    >
+                        Load More</Button>
                 </div>
 
             </Container>
--- src/components/CommentNew.js
+++ src/components/CommentNew.js
@@ -3,7 +3,38 @@ import UserImg from './media/UserImg.svg'
 import "./styles/CommentNew.css"
 import heartUnlike from "./media/heartUnlike.svg";
 import heartLike from "./media/heartLike.svg";
+import { Overlay, Tooltip } from 'react-bootstrap';
+
 export class CommentNew extends Component {
+    constructor() {
+        super();
+        this.likeRef = React.createRef();
+        this.state = {
+            showToolTip: false,
+            windowWidth: window.innerWidth
+        }
+
+    }
+
+    handleResize = () => {
+        this.setState({ windowWidth: window.innerWidth });
+    };
+
+    componentDidMount() {
+        window.addEventListener("resize", this.handleResize);
+    }
+
+    handleHoverOnLikes = () => {
+        this.setState({
+            showToolTip: true
+        })
+    }
+
+    handleHoverAwayFromLikes = () => {
+        this.setState({
+            showToolTip: false
+        })
+    }
 
     handleLikeButtonClick = (e) => {
         let operation;
@@ -15,6 +46,28 @@ export class CommentNew extends Component {
 
         let index = parseInt(e.target.getAttribute("index"));
         this.props.updateLikeCount(index, operation);
+    }
+
+    formattLikesCount = (num) => {
+        let digits = 2;//no of digits after decimal
+
+        var si = [
+            { value: 1, symbol: "" },
+            { value: 1E3, symbol: "K" },
+            { value: 1E6, symbol: "M" },
+            { value: 1E9, symbol: "G" },
+            { value: 1E12, symbol: "T" },
+            { value: 1E15, symbol: "P" },
+            { value: 1E18, symbol: "E" }
+        ];
+        var rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+        var i;
+        for (i = si.length - 1; i > 0; i--) {
+            if (num >= si[i].value) {
+                break;
+            }
+        }
+        return (num / si[i].value).toFixed(digits).replace(rx, "$1") + si[i].symbol;
     }
 
     render() {
@@ -34,15 +87,26 @@ export class CommentNew extends Component {
 
                     <div className="commentFooter">
                         <span
+                            onMouseEnter={this.handleHoverOnLikes}
+                            onMouseLeave={this.handleHoverAwayFromLikes}
+                            ref={this.likeRef}
                             index={this.props.index}
                             onClick={this.handleLikeButtonClick}
                             style={{ fontWeight: this.props.isLiked ? 700 : null }}
                         >
-                            {this.props.likes === 0 ? null : this.props.likes}
+                            {this.props.likes === 0 ? null : this.formattLikesCount(this.props.likes)}
 
                         </span>
+                        <Overlay
+                            target={this.likeRef.current}
+                            show={this.state.showToolTip}
+                            placement={this.state.windowWidth <= 575 ? "top" : "left"}
+                        >
+                            <Tooltip>{this.props.likes}</Tooltip>
+                        </Overlay>
 
                         <img index={this.props.index} onClick={this.handleLikeButtonClick}
+                            style={{ marginLeft: this.props.likes === 0 ? 0 : "5px" }}
                             src={this.props.isLiked ? heartLike : heartUnlike} alt="unlike-icon" />
                     </div>
 
